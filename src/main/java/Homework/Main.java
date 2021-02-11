31e51a75714372f9de110d1ebec01ac4886f57e8
SergTaranenko <rates-13@inbox.ru>
2021-02-11 08:41:13 +0300

    1

--- src/main/java/Homework/Main.java
+++ src/main/java/Homework/Main.java
@@ -1,4 +1,42 @@
 package Homework;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeSet;
+
 public class Main {
+    private static final String words =
+            "Lie upon the lie " +
+                    "upon the lie " +
+                    "upon the lie " +
+                    "upon the lie " +
+                    "And it all comes down to this " +
+                    "That a pig is a pig is a pig is a pig";
+
+    private static TreeSet<String> getWords(String[] arr) {
+        return new TreeSet<>(Arrays.asList(arr));
+    }
+
+    private static HashMap<String, Integer> getWordsCount(String[] arr) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            String word = arr[i];
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getWords(words.toLowerCase().split(" ")));
+        System.out.println(getWordsCount(words.toLowerCase().split(" ")));
+
+        Phonebook phonebook = new Phonebook();
+        phonebook.add("Ivanov", "01");
+        phonebook.add("Petrov", "02");
+        phonebook.add("Sidorov", "03");
+
+         System.out.println("phone Ivanov: " + phonebook.getPhones("Ivanov"));
+        System.out.println("phone Petrov: " + phonebook.getPhones("Petrov"));
+        System.out.println("phone Sidorov: " + phonebook.getPhones("Sidorov"));
+    }
 }
---
+++ src/main/java/Homework/Person.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class Person {
+    String name;
+    String phone;
+
+
+    public Person(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
